daa4259f1a53c6764393ae7e7bb76cb14d1f97b6
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-14 12:05:03 +0800

    feat: add buckets property to class HashMap

--- hash-map.js
+++ hash-map.js
@@ -1,6 +1,11 @@
-class HashMap {
-    constructor(loadFactor, capacity) {
+export class HashMap {
+    constructor(loadFactor = 0.75, capacity = 16) {
         this.loadFactor = loadFactor;
         this.capacity = capacity;
+        this.buckets = new Array(capacity);
     }
 }
+
+const test = new HashMap();
+console.log(test);
+console.log(test.buckets);
