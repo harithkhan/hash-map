2c7428332d234294444bfd890cd47a44e755159e
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-14 17:55:39 +0800

    feat: add incomplete function set in HashMap Add incomplete function set in class HashMap that, for now, can set new properties in a HashMap instance bucket if a bucket is empty, or if it is not empty but with the same key. No functionality added yet for if the hash is the same but the key is different, and no consideration is given yet for load factor and capacity changes.

--- hash-map.js
+++ hash-map.js
@@ -1,3 +1,5 @@
+import { LinkedList } from "./linked-list.js";
+
 export class HashMap {
     constructor(loadFactor = 0.75, capacity = 16) {
         this.loadFactor = loadFactor;
@@ -14,8 +16,38 @@ export class HashMap {
         }
         return hashCode;
     }
+
+    set(key, value) {
+        const { buckets } = this;
+        const index = this.hash(key);
+        console.log(`Index: ${index}`);
+        if (!buckets[index]) {
+            const newLinkedList = new LinkedList();
+            newLinkedList.append(key, value);
+            buckets[index] = [newLinkedList];
+            return;
+        }
+        const existingList = buckets[index][0];
+        console.log(`buckets: ${buckets}`)
+        console.log(`existing: ${existingList}`)
+        let current = existingList.head;
+        console.log(`current: ${current}`);
+        while (current !== null) {
+            if (current.key === key) {
+                current.value = value;
+                return;
+            }
+            current = current.nextNode;
+        }
+
+        // if (existingList.key !== key) {
+
+        // }
+    }
 }
 
 const test = new HashMap();
+test.set("Apples", 5);
+test.set("Apples", 7);
 console.log(test);
-console.log(test.buckets);
+console.log(JSON.stringify(test.buckets, null, 2));
